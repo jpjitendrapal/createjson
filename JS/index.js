a5895afc246fd75e42b1182696191bcbdc19582b
jitendra pal <jitendra.pal@MAC-6FVH7.local>
2018-10-05 17:18:25 +0530

    updated

--- JS/index.js
+++ JS/index.js
@@ -18,11 +18,11 @@ Question.prototype = {
     },
     renderQuestion: function () {
         var htm;
-        htm = "<div data-id='" + this.id + "' class='question'>" +
-              "<div onclick='qlist.deleteQuestion(" + this.id + ");updateHtml();' data-qid='"+ this.id +"' class='delete'>X</div>"+
-            "<input class='q-text' type='text' value='" + this.text + "'>" +
-            this.renderOptions() +
-            "</div>";
+        htm = "<fieldset><legend class='b bg-light-yellow'>Question " + this.id + ":</legend>" + 
+            "<div data-id='" + this.id + "' class='question'>" +
+            "<div onclick='qlist.deleteQuestion(" + this.id + ");updateHtml();' data-qid='" + this.id +"' class='delete'>X</div>" +
+            "<input class='q-text ba lh-copy' type='text' value='" + this.text + "'>" + this.renderOptions() +
+            "</div></fieldset>";
 
         return htm;
     },
@@ -31,7 +31,7 @@ Question.prototype = {
         htm = "<div class='options'>";
         for (var i = 0; i < this.options.length; i++) {
             htm += "<div data-id='" + this.options[i].id + "' class='ans'>" +
-                "<input data-ansid='" + this.options[i].id + "' type='radio' name='option" + this.id + "' /><input class='ans-text' type='text' value='" + this.options[i].text + "'/></div>";
+                "<br><input data-ansid='" + this.options[i].id + "' type='radio' name='option" + this.id + "' />&nbsp;&nbsp;<input class='ans-text ba lh-copy' type='text' value='" + this.options[i].text + "'/></div>";
         }
         htm += "</div>";
         return htm;
@@ -46,7 +46,9 @@ Question.prototype = {
         var ansnode;
         for (var i = 0; i < this.options.length; i++) {
             ansnode = document.querySelectorAll(".question[data-id='" + this.id + "'] .options [data-id='" + this.options[i].id + "'] .ans-text");
-            this.options[i].text = ansnode[0].value;
+            if(ansnode.length > 0){
+                this.options[i].text = ansnode[0].value;
+            }
         }
     }
 }
@@ -59,12 +61,15 @@ function QuestionList() {
 QuestionList.prototype = {
     idlist: [],
     addQuestion: function (question) {
+        if(qlist){
+            qlist.updateQuestions();
+        }
         if (question) {
             this.questions.push(question);
         } else {
             this.idlist.push(this.idlist.length+1);
             var id = this.idlist.length;
-            question = new Question(id, "This is your question?", [{ id: 1, text: "answer1" }, { id: 2, text: "answer 2" },{ id: 3, text: "answer 3" },{ id: 4, text: "answer 4" }]);
+            question = new Question(id, "This is your question?", [{ id: 1, text: "answer 1" }, { id: 2, text: "answer 2" },{ id: 3, text: "answer 3" },{ id: 4, text: "answer 4" }]);
         }
         this.questions.push(question);
     },
@@ -100,7 +105,6 @@ function updateHtml(){
 
 var qlist = new QuestionList();
 qlist.addQuestion();
-qlist.addQuestion();
 updateHtml();
 
 
@@ -115,11 +119,8 @@ function createJson() {
         var ops = qJson.questions[i].options;
         for (var j = 0; j < ops.length; j++) {
             opsText = ops[j].text.trim();
-            if(opsText) {
-                ops[j] = opsText;
-            } else {
-                 ops.splice(j,1);
-            }
+            ops[j] = opsText;
+            
         }
         checkedAns=document.querySelectorAll("[name='option"+ qJson.questions[i].id +"']:checked");
         qJson.questions[i]["ans"] = (checkedAns && checkedAns.length >0) ? checkedAns[0].dataset["ansid"] : "-1";
@@ -130,4 +131,21 @@ function createJson() {
 
 function addOtherTextFileds(obj){
     obj["id"] = document.getElementById("quizId").value;
+    obj["landingPage"] = {};
+    obj["landingPage"]["title"] = document.getElementById("landingPage_title").value;
+    obj["landingPage"]["subTitle"] = document.getElementById("landingPage_subTitle").value;
+    obj["landingPage"]["quizTitle"] = document.getElementById("landingPage_quizTitle").value;
+    obj["landingPage"]["quizCompleteSubTitle"] = document.getElementById("landingPage_quizCompleteSubTitle").value;
+    obj["successPage"] = {};
+    obj["successPage"]["title"] = document.getElementById("successPage_title").value;
+    obj["successPage"]["subTitle"] = document.getElementById("successPage_subTitle").value;
+    obj["successPage"]["wonText"] = document.getElementById("successPage_wonText").value;
+    obj["successPage"]["coupon"] = document.getElementById("successPage_coupon").value;
+    obj["successPage"]["expText"] = document.getElementById("successPage_expText").value;
+    obj["errorPage"] = {};
+    obj["errorPage"]["title"] = document.getElementById("errorPage_title").value;
+    obj["errorPage"]["subTitle"] = document.getElementById("errorPage_subTitle").value;
+    obj["config"] = {};
+    obj["config"]["nextQtime"] = parseInt(document.getElementById("nextQtime").value, 10);
+    obj["config"]["wrongQtime"] = parseInt(document.getElementById("wrongQtime").value, 10);
 }
